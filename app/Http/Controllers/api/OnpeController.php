9dcaf207a0c9be7cf9613f7e31aff10807ef5d53
Dennis <andrewdelacruz547@gmail.com>
2026-09-15 21:59:06 -0500

    primer avance de lo de clases

--- app/Http/Controllers/api/OnpeController.php
+++ app/Http/Controllers/api/OnpeController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class ApiController extends Controller
+class OnpeController extends Controller
 {
+
+    public function grupo_votacion($grupo_votacion)
+    {
+        $acta = DB::select('call sp_getGrupoVotacion(?)', [$grupo_votacion]);
+
+        $success = isset($acta);
+        $status = $success ? 200 : 404;
+
+        $acta = [
+            'success' => $success,
+            'acta' => $success ? $acta : null,
+            'message' => $success ? 'Acta encontrada' : 'No existe esta acta',
+            'status' => $status
+        ];
+        return response()->json($acta, $status);
+    }
     /**
      * Display a listing of the resource.
      */
